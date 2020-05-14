004caa4132c805062b87e169d770e9b33ac19c82
Tyler Matijevich <tyler.matijevich@br-automation.com>
2020-05-14 17:50:42 -0500

    Import MathTools, Create PathKin and the first functions

---
+++ PathKin.fun
@@ -0,0 +1,11 @@
+
+FUNCTION Kin_GetAcc : BOOL
+	VAR_INPUT
+		dt : REAL;
+		dx : REAL;
+		v0 : REAL;
+		vf : REAL;
+		vmin : REAL;
+		vmax : REAL;
+	END_VAR
+END_FUNCTION
---
+++ get_acc.c
@@ -0,0 +1,11 @@
+/*********************************************************************************
+ * Copyright: B&R Industrial Automation 
+ * Author:    Tyler Matijevich
+ * Created:   May 14, 2020/5:46 PM 
+ *********************************************************************************/ 
+#include "PathKin.h"
+#include <math.h>
+ 
+plcbit Kin_GetAcc(float dt, float dx, float v0, float vf, float vmin, float vmax) {
+	return 1;
+}
