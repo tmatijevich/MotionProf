7846c12acb71304dcb00b1ddd908cd963b32840b
Tyler Matijevich <tyler.matijevich@br-automation.com>
2021-03-24 11:49:39 -0500

    Declare GetAcc function and create source file

---
+++ GetAcc.c
@@ -0,0 +1,19 @@
+/*******************************************************************************
+ * File: GetAcc.c
+ * Author: Tyler Matijevich
+ * Date: 2020-04-10
+*******************************************************************************/
+
+// <file> - System header files
+// "file" - Program header files
+#include <PathPlan.h>
+#include "PathPlanMain.h"
+
+/* Determine the minimum acceleration to change velocity in time over a distance */
+DINT GetAcc(REAL dt, REAL dx, REAL v0, REAL vf, REAL vmin, REAL vmax, struct PathPlanBaseSolutionType* Solution) {
+	
+	
+
+	return PATH_ERROR_NONE;
+	
+} // End function definition
--- PathPlan.fun
+++ PathPlan.fun
@@ -1,4 +1,18 @@
 
+FUNCTION GetAcc : DINT (*Determine the minimum acceleration to change velocity in time over a distance*)
+	VAR_INPUT
+		dt : REAL; (*Time duration*)
+		dx : REAL; (*Distance*)
+		v0 : REAL; (*Initial velocity*)
+		vf : REAL; (*Final velocity*)
+		vmin : REAL; (*Minimum velocity*)
+		vmax : REAL; (*Maximum velocity*)
+	END_VAR
+	VAR_IN_OUT
+		Solution : PathPlanBaseSolutionType; (*Solution reference*)
+	END_VAR
+END_FUNCTION
+
 FUNCTION GetPoint : DINT (*Determine the point on a piecewise linear velocity profile*)
 	VAR_INPUT
 		InitialPosition : REAL; (*Initial position of the velocity profile*)
