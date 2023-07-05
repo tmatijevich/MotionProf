9082b9d96bb5e06c16d2478b54bec7c0ad395cae
Tyler Matijevich <tyler.matijevich@br-automation.com>
2023-07-05 17:08:31 -0500

    Remove windows shortcut in favor of batch file, improve make file

---
+++ Octave/launch.bat
@@ -0,0 +1,1 @@
+start "Octave CLI" /wait "C:\Program Files\GNU Octave\Octave-8.2.0\octave-launch.exe" --no-gui
--- Octave/make.m
+++ Octave/make.m
@@ -1,8 +1,12 @@
+tic
 clc
 clear all
+w = warning("off", "all");
 delete *.oct
+warning(w)
 printf("Build OctPathPlanRoots\n")
 mkoctfile octpathplanroots.cc ../Source/roots.c -DOCTAVE_BUILD
 printf("Build OctPathPlanAcc\n")
 mkoctfile OctPathPlanAcc.cc ../Source/acc.c ../Source/roots.c -DOCTAVE_BUILD
 printf("Build attempt finished\n")
+toc
