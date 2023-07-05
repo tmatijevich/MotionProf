13a6fb098d741c159a5d6cadecb64a9351e03e8a
Tyler Matijevich <tyler.matijevich@br-automation.com>
2023-07-05 16:17:14 -0500

    Create octave function for PathPlanAcc, new use of structure, update header and make

--- Octave/make.m
+++ Octave/make.m
@@ -1,4 +1,8 @@
 clc
 clear all
 delete *.oct
+printf("Build OctPathPlanRoots\n")
 mkoctfile octpathplanroots.cc ../Source/roots.c -DOCTAVE_BUILD
+printf("Build OctPathPlanAcc\n")
+mkoctfile OctPathPlanAcc.cc ../Source/acc.c ../Source/roots.c -DOCTAVE_BUILD
+printf("Build attempt finished\n")
--- Octave/octave.h
+++ Octave/octave.h
@@ -11,12 +11,33 @@ extern "C"
 
 #include <stdint.h>
 
+typedef enum PathPlanMoveEnum {
+  PATHPLAN_MOVE_NONE = 0,
+  PATHPLAN_MOVE_DEC_ACC = 1,
+  PATHPLAN_MOVE_DEC_ACC_SATURATED = 2,
+  PATHPLAN_MOVE_ACC_DEC = 3,
+  PATHPLAN_MOVE_ACC_DEC_SATURATED = 4,
+  PATHPLAN_MOVE_DEC_DEC = 10,
+  PATHPLAN_MOVE_ACC_ACC = 11
+} PathPlanMoveEnum;
+
 typedef struct PathPlanRootsOutputType {
   double r_1;
   double r_2;
 } PathPlanRootsOutputType;
 
-int32_t PathPlanRoots(double p_2, double p_1, double p_0, PathPlanRootsOutputType *output);
+typedef struct PathPlanBaseOutputType {
+  double t_[4];
+  double dx;
+  double v_[4];
+  double a;
+  enum PathPlanMoveEnum move;
+} PathPlanBaseOutputType;
+
+int32_t PathPlanRoots(double p_2, double p_1, double p_0,
+                      PathPlanRootsOutputType *output);
+int32_t PathPlanAcc(double dt, double dx, double v_0, double v_f, double v_min,
+                    double v_max, PathPlanBaseOutputType *output);
 
 #ifdef __cplusplus
 };
